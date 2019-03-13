d3cd19afd0e0d0ea2e068fc0f153e323f4f56a46
madeirak <haoran4@126.com>
2019-03-13 16:48:52 +0800

    temp
    123

--- manual_test/Amodel_test.py
+++ manual_test/Amodel_test.py
@@ -11,13 +11,31 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
 
 # 0.准备解码所需字典
 from utils import get_data, data_hparams
+
+#data_args = data_hparams()
+
+# 0.准备验证所需数据------------------------------
 data_args = data_hparams()
-train_data = get_data(data_args)
+data_args.data_type = 'dev'
+data_args.data_path = '../dataset/'
+data_args.thchs30 = True
+data_args.aishell = True
+data_args.prime = False
+data_args.stcmd = False
+data_args.batch_size = 4
+data_args.data_length = None #作者做实验时写小一些看效果用的，正常使用设为None
+#data_args.data_length = 10
+data_args.shuffle = True
+dev_data = get_data(data_args)
+
+
+#train_data = get_data(data_args)
 # 1.声学模型-----------------------------------
 from model_speech.cnn_ctc import Am, am_hparams
 
 am_args = am_hparams()
-am_args.vocab_size = len(train_data.am_vocab)
+am_args.vocab_size = len(dev_data.am_vocab)
+#am_args.vocab_size = len(train_data.am_vocab)
 am = Am(am_args)
 print('loading acoustic model...')
 am.ctc_model.load_weights('G:/DeepSpeechRecognition/logs_am/model.h5')#从绝对路径的检查点恢复权重数据
@@ -53,7 +71,7 @@ new_wav_data_lst[0, :wav_data_lst[0].shape[0], :, 0] = wav_data_lst[0]    #"data
 #new_wav_data_lst = tf.expand_dims(new_wav_data_lst, 0)#3d->4d
 
 result = am.model.predict(new_wav_data_lst, steps=1)#steps预测周期结束前的总步骤数(样品批次)，predict返回numpy数组类型的预测
-
-_, text = decode_ctc(result, train_data.am_vocab)  # num2pny
+_, text = decode_ctc(result, dev_data.am_vocab)  # num2pny
+#_, text = decode_ctc(result, train_data.am_vocab)  # num2pny
 text = ' '.join(text)  # 以空格为分隔符合将多元素列表text合并成一个字符串
 print('文本结果：', text)
